ad0b3901558e39334afbc271851d17a7e75bcf75
Joakim Vanebo <Pjatte@stud.noroff.no>
2022-11-10 14:04:59 +0100

    cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
+    video: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.env = {
+        baseUrl: 'http://127.0.0.1:5173/',
+        ...process.env,
+        ...config.env,
+      };
+      return config;
     },
   },
 });
